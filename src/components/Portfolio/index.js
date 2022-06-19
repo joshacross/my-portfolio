b9bd156fb9171daea62792df2c412392bf3498de
joshacross <joshuaalexandercross@gmail.com>
2022-06-19 09:06:37 -0400

    added web links

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -26,10 +26,10 @@ function Portfolio() {
                         <Card.Text>
                         A fully responsive, full-stack, client-side Node.js web application that streamlines legal contracts for entrepreneurs. Built with Express.js, Handlebars.js Template Engine, and MySQL, deployed on Heroku Server.
                         </Card.Text>
-                    <Button variant="dark" className='justify-center'>W E B S I T E</Button>
+                    <Button href="https://infinite-shore-33875.herokuapp.com/" variant="dark" className='justify-center'>W E B S I T E</Button>
                     <br></br>
                     <br></br>
-                    <Button variant="dark">G I T H U B</Button>
+                    <Button href="https://github.com/joshacross/devtrax" variant="dark">G I T H U B</Button>
                 </Card.Body>
             </Card>
             </Col>
@@ -41,10 +41,10 @@ function Portfolio() {
                         <Card.Text>
                         Taskmaster-pro v2.0 is an updated version of Taskmaster-pro using JQuery, JQuery UI Bootstrap, Moment.JS, and other third-party API's via CDN. Legacy code was refactored to manipulate the DOM using jQuery, styled using bootstrap and custom styles to create responsive and advanced-UI design, interactive features were added using JQuery UI, simplified date and time functionality using MomentJS to account for immenent and past due dates and updated fonts using Google fonts CDN.
                         </Card.Text>        
-                        <Button variant="dark">W E B S I T E</Button>
+                        <Button href="https://joshacross.github.io/taskmaster-pro/" variant="dark">W E B S I T E</Button>
                         <br></br>
                         <br></br>
-                        <Button variant="dark">G I T H U B</Button>
+                        <Button href="https://github.com/joshacross/taskmaster-pro" variant="dark">G I T H U B</Button>
                 </Card.Body>
             </Card>
             </Col>
@@ -57,10 +57,10 @@ function Portfolio() {
                     <Card.Title>Motivation Manager</Card.Title>
                         <Card.Text>
                         The following application is a mobile-forward task-management solution for entrepreneurs and business owners who want to double their output, surpass their business goals, and finish each week feeling satisfied with their progress, all while enjoying more time for what they care about the most.                        </Card.Text>
-                        <Button variant="dark">W E B S I T E</Button>
+                        <Button href="https://github.com/JohnBanas/motivation-manager" variant="dark">W E B S I T E</Button>
                         <br></br>
                         <br></br>
-                        <Button variant="dark">G I T H U B</Button>
+                        <Button href="https://johnbanas.github.io/motivation-manager/" variant="dark">G I T H U B</Button>
                 </Card.Body>
             </Card>
             </Col>
@@ -72,10 +72,10 @@ function Portfolio() {
                         <Card.Text>
                             A lightning fast photography portfolio website built with React, Javascript, and JSX. Also, what seperates Oh Snap! from the normal portfolio is it's ability to map through various categories of photos by manipulating state and utilizing the virtual DOM.
                         </Card.Text>
-                        <Button variant="dark">W E B S I T E</Button>
+                        <Button href="https://joshacross.github.io/photo-port/" variant="dark">W E B S I T E</Button>
                         <br></br>
                         <br></br>
-                        <Button variant="dark">G I T H U B</Button>
+                        <Button href='https://github.com/joshacross/photo-port' variant="dark">G I T H U B</Button>
                 </Card.Body>
             </Card>
             </Col>
