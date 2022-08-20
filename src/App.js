9c4e8def7e0afe7f052f490dee51bb0ea7386c0a
joshacross <joshuaalexandercross@gmail.com>
2022-08-19 21:11:29 -0500

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import PortfolioPage from "./pages/PortfolioPage";
-import ContactPage from "./pages/ContactPage";
-import ResumePage from "./pages/ResumePage";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import PortfolioPage from './pages/PortfolioPage';
+import ContactPage from './pages/ContactPage';
+import ResumePage from './pages/ResumePage';
 import NoMatch from './pages/NoMatch';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 
 function App() {
-  return (
-      <BrowserRouter basename="/my-portfolio">
-        <header>
-            <Navigation />
-        </header>
-        <Routes>
-        
-            <Route index element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/portfolio" element={<PortfolioPage />} />
-            <Route path="/resume" element={<ResumePage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route component={NoMatch} />
-        </Routes>
-        <footer>
-            <Footer />
-        </footer>
-      </BrowserRouter>
-  );
+     return (
+          <Router basename='/my-portfolio'>
+               <header>
+                    <Navigation />
+               </header>
+               <Routes>
+                    <Route exact path='/' element={<HomePage />} />
+                    <Route path='/about' element={<AboutPage />} />
+                    <Route path='/portfolio' element={<PortfolioPage />} />
+                    <Route path='/resume' element={<ResumePage />} />
+                    <Route path='/contact' element={<ContactPage />} />
+                    <Route component={NoMatch} />
+               </Routes>
+               <footer>
+                    <Footer />
+               </footer>
+          </Router>
+     );
 }
 
 export default App;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,48 +2,71 @@ import React from 'react';
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 function Navigation() {
+     return (
+          <>
+               <nav>
+                    <Navbar
+                         collapseOnSelect
+                         expand='lg'
+                         className='dark'
+                         bg='dark'
+                         variant='dark'>
+                         <Container fluid>
+                              <Navbar.Brand href='/my-portfolio'>
+                                   <h1>Joshua Cross</h1> <pre>portfolio</pre>
+                              </Navbar.Brand>
 
-    return (
-        <>
-        <nav>
-        <Navbar collapseOnSelect expand="lg" className="dark" bg="dark" variant="dark">
-            <Container fluid>
-            <Navbar.Brand href="/my-portfolio"><h1>Joshua Cross</h1> <pre>portfolio</pre></Navbar.Brand>
-
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href='#portfolio'>Portfolio</Nav.Link>
-                        <NavDropdown title="Projects" id="collapsible-nav-dropdown">
-                                <NavDropdown.Item  href='#react-photo-port'>
-                                    ReactApp - Oh Snap!
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href='#task-master-pro'>
-                                    Task-Master Pro v2.0
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href='#project-devtrax'>
-                                    Full Stack Application - devtrax
-                                </NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#project-motivation-manager">
-                                        Group Project - Motivation Manager
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href='#augmented-retail'>
-                                    Group Project - Augmented Retail
-                                </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    <Nav>
-                            <Nav.Link href="#contact">Contact</Nav.Link>
-                            <Nav.Link href="https://joshacross.github.io/portfolio/assets/files/Joshua%20Cross%20Resume_Portfolio%20(3).pdf">Resume</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-        </nav>
-        </>
-
-    )}
+                              <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                              <Navbar.Collapse id='responsive-navbar-nav'>
+                                   <Nav className='mr-auto'>
+                                        <Nav.Link
+                                             href='/my-portfolio/about'
+                                             activeClassName='active'>
+                                             About
+                                        </Nav.Link>
+                                        <Nav.Link
+                                             href='/my-portfolio/portfolio'
+                                             activeClassName='active'>
+                                             Portfolio
+                                        </Nav.Link>
+                                        <NavDropdown
+                                             title='Projects'
+                                             id='collapsible-nav-dropdown'>
+                                             <NavDropdown.Item href='#react-photo-port'>
+                                                  ReactApp - Oh Snap!
+                                             </NavDropdown.Item>
+                                             <NavDropdown.Item href='#task-master-pro'>
+                                                  Task-Master Pro v2.0
+                                             </NavDropdown.Item>
+                                             <NavDropdown.Item href='#project-devtrax'>
+                                                  Full Stack Application -
+                                                  devtrax
+                                             </NavDropdown.Item>
+                                             <NavDropdown.Divider />
+                                             <NavDropdown.Item href='#project-motivation-manager'>
+                                                  Group Project - Motivation
+                                                  Manager
+                                             </NavDropdown.Item>
+                                             <NavDropdown.Item href='#augmented-retail'>
+                                                  Group Project - Augmented
+                                                  Retail
+                                             </NavDropdown.Item>
+                                        </NavDropdown>
+                                   </Nav>
+                                   <Nav>
+                                        <Nav.Link to='/contact'>
+                                             Contact
+                                        </Nav.Link>
+                                        <Nav.Link href='https://joshacross.github.io/portfolio/assets/files/Joshua%20Cross%20Resume_Portfolio%20(3).pdf'>
+                                             Resume
+                                        </Nav.Link>
+                                   </Nav>
+                              </Navbar.Collapse>
+                         </Container>
+                    </Navbar>
+               </nav>
+          </>
+     );
+}
 
 export default Navigation;
